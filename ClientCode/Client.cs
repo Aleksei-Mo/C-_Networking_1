daf45b90610eb0ffde97bafc2f79a211eaa28bca
Alex_Mo <doj20@mail.ru>
2023-12-09 20:53:50 +0300

    Rev 1

---
+++ ClientCode/Client.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using ServerCode;
+
+
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SentMessage(args[0], args[1]);
+        }
+
+
+        public static void SentMessage(string From, string ip)
+        {
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(ip), 12345);
+            while (true)
+            {
+                string messageText;
+                do
+                {
+                    //Console.Clear();
+                    Console.WriteLine("Введите сообщение.");
+                    messageText = Console.ReadLine();
+
+                }
+                while (string.IsNullOrEmpty(messageText));
+                Message message = new Message() { Text = messageText, NicknameFrom = From, NicknameTo = "Server", DateTime = DateTime.Now };
+                string json = message.SerializeMessageToJson();
+                byte[] data = Encoding.UTF8.GetBytes(json);
+                int messageSize = udpClient.Send(data, data.Length, iPEndPoint);
+
+                Console.WriteLine($"Client has sent the message with lenght {messageSize} to the Server! Waiting for the Server feedback.");
+                //Console.WriteLine($"The message {message.Text} has been sent successfuly!");
+                ServerFeedbackToClient(messageSize);
+            }
+
+        }
+
+        public static void ServerFeedbackToClient(int msgSize)
+        {
+            UdpClient udpClient = new UdpClient(1234);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("Клиент ждет обратную связь от сервера.");
+            while (true)
+            {
+                byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                if (buffer == null) break;
+                var messageText = Encoding.UTF8.GetString(buffer);
+                int messageSize = int.Parse(JsonSerializer.Deserialize<String>(messageText));
+                if (msgSize==messageSize)
+                {
+                   Console.WriteLine("The message has been recieved by Server successfully!");
+                   return; 
+                }
+            }
+        }
+    }
+}
---
+++ ServerCode/Server.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+
+
+namespace ServerCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server();
+        }
+
+        /* public void task1()
+         {
+             Message msg = new Message() { Text = "Hello", DateTime = DateTime.Now, NicknameFrom = "Aleksei", NicknameTo = "All" };
+             string json = msg.SerializeMessageToJson();
+             Console.WriteLine(json);
+             Message? msgDeserialized = Message.DeserializeFromJson(json);
+         }*/
+
+        public static void Server()
+        {
+            UdpClient udpClient = new UdpClient(12345);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("Сервер ждет сообщение от клиента");
+            while (true)
+            {
+                byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                if (buffer == null) break;
+                Console.WriteLine($"Size of message is {buffer.Length}");
+                var messageText = Encoding.UTF8.GetString(buffer);
+                Message message = Message.DeserializeFromJson(messageText);
+                message.Print();
+                ServerFeedbackToClient(buffer.Length);
+            }
+        }
+
+        public static void ServerFeedbackToClient(int messageSize)
+        {
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.2"), 1234);
+            string messageText = messageSize.ToString();//send to client the recieved message size
+            string json = JsonSerializer.Serialize(messageText);
+            byte[] data = Encoding.UTF8.GetBytes(json);
+            udpClient.Send(data, data.Length, iPEndPoint);
+            
+        }
+
+    }
+}
